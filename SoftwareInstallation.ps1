211439309ccbe92b2e3a58b6e24dea95482cc89a
Kevin Rincon <kevinrincon8@gmail.com>
2024-01-02 23:38:55 -0500

    Inicializacion de archivos de configuracion de equipos personales windows

---
+++ SoftwareInstallation.ps1
@@ -0,0 +1,10 @@
+winget install --id=Microsoft.VisualStudioCode  -e
+winget install --id CoreyButler.NVMforWindows -e
+winget install --id=Git.Git  -e
+winget install --id=Microsoft.WindowsTerminal  -e
+winget install --id=Google.Chrome  -e
+winget install --id=lukehaas.RunJS  -e
+winget install --id=Notion.Notion  -e
+winget install --id=Discord.Discord  -e
+winget install --id=Spotify.Spotify  -e
+winget install --id=WhatsApp.WhatsApp  -e
